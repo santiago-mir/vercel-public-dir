68ed34cca6bc5f5120c95a6b467f782271abc8fb
Santiago <san_chan97@hotmail.com>
2025-01-06 16:09:14 -0300

    update post method

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -1,4 +1,6 @@
-export default function auth(req, res) {
+import { post } from "micro-method-router";
+
+export default function post(req, res) {
   const email = req.body.email;
   res.status(200).json({
     body: req.body,
